355e6073c454733e65939dd3c1ca60b6fb65a79f
Kang-SeoHyun <kang061020@gmail.com>
2023-05-29 01:13:12 +0900

    Change window

---
+++ 코드/test/test/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!:) ");
+Console.WriteLine("What's your name?");
+
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+Console.WriteLine();
+Console.WriteLine($"Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.Write($"Press any key to exit..");
